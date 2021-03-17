764a56e1160e6c88f0cb81dbbaed5bc1a1b3e06a
Yura Husak <yura.gusak7@gmail.com>
2021-03-17 21:51:12 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+
+    class InnerClass
+    {
+        static void Main()
+        {
+            Lion lion = new Lion();
+            lion.LionVoice();
+
+            Bear bear = new Bear();
+            bear.BearVoice();
+            bear.Voice();
+        }
+    }
+
+    abstract class Animal
+    {
+        public virtual void Voice()
+        {
+            Console.WriteLine("VOICE");
+        }
+    }
+
+    class Lion
+    {
+        public void LionVoice()
+        {
+            Console.WriteLine("LION VOICE");
+        }
+
+        ~Lion()
+        {
+            Console.WriteLine("Lion class destructor has been called.");
+        }
+    }
+
+    class Bear: Animal
+    {
+        public void BearVoice()
+        {
+            Console.WriteLine("BEAR VOICE");
+        }
+
+        public override void Voice()
+        {
+            Console.WriteLine("VOICE??");
+        }
+    }
+}
